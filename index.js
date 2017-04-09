4bf1e5bf1397ae77bf30a8aafbde1529bf05ed66
Vishrut Shah <vissha@microsoft.com>
2017-04-09 13:24:51 -0700

    removing multer

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var oav = require('oav');
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-var multer = require('multer');
+// var multer = require('multer');
+
+var app = express();
 // app.use(bodyParser.json()); // for parsing application/json
 // app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
