04246664c5a8aac187cae47e5398ae638fe4296a
Vishrut Shah <vissha@microsoft.com>
2017-04-08 23:06:42 -0700

    Enable express server

--- index.js
+++ index.js
@@ -5,31 +5,31 @@
 // }).catch((err) => {
 //     console.dir(err, { depth: null, colors: true });
 // });
-// var express = require('express');
-// var app = express();
+var express = require('express');
+var app = express();
 
-// app.get('/', function (req, res) {
-//    res.send('Hello World');
-// })
-
-// var port = process.env.PORT || 1337;
-// var server = app.listen(port, function () {
-//    var host = server.address().address
-//    var port = server.address().port
-   
-//    console.log("Example app listening at http://%s:%s", host, port)
-// })
-
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure!");
-
-});
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
 
 var port = process.env.PORT || 1337;
-server.listen(port);
+var server = app.listen(port, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
 
-console.log("Server running at http://localhost:%d", port);
+// var http = require('http');
+
+// var server = http.createServer(function(request, response) {
+
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello Azure!");
+
+// });
+
+// var port = process.env.PORT || 1337;
+// server.listen(port);
+
+// console.log("Server running at http://localhost:%d", port);
