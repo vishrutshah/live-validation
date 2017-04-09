50cad79a3a70c6f2179976484657ed11cec78e38
Vishrut Shah <vissha@microsoft.com>
2017-04-09 12:35:41 -0700

    Adding spec validation endpoint

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-// var oav = require('oav');
-
-// oav.validateExamples("https://github.com/Azure/azure-rest-api-specs/blob/master/arm-redis/2016-04-01/swagger/redis.json").then((result) => {
-//     console.dir(result, { depth: null, colors: true });
-// }).catch((err) => {
-//     console.dir(err, { depth: null, colors: true });
-// });
+var oav = require('oav');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+    res.send('Hello World');
+})
+
+// This responds a POST request for the homepage
+app.post('/semantic/validate', function (req, res) {
+    console.log("Got a POST request for the /semantic/validate");
+    console.log(req.body);
+    console.log();
+    if (req.body && req.body.swagger) {
+        let swagger = req.body.swagger
+        oav.validateSpec(swagger).then((result) => {
+            console.dir(result, { depth: null, colors: true });
+            res.send(result);
+        }).catch((err) => {
+            console.dir(err, { depth: null, colors: true });
+            res.send(err);
+        });
+    }
 })
 
 var port = process.env.PORT || 1337;
 var server = app.listen(port, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
 })
