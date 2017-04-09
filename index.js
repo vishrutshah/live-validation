0215e9c626d1f2d67151325675e44b4cf70bf6ea
Vishrut Shah <vissha@microsoft.com>
2017-04-08 23:07:59 -0700

    Removing sample http server

--- index.js
+++ index.js
@@ -19,17 +19,3 @@ var server = app.listen(port, function () {
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
-
-// var http = require('http');
-
-// var server = http.createServer(function(request, response) {
-
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello Azure!");
-
-// });
-
-// var port = process.env.PORT || 1337;
-// server.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
