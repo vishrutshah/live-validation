99c7cde52e2c3371b23612bfa1960b379c0814ed
Vishrut Shah <vissha@microsoft.com>
2017-04-09 13:11:13 -0700

    removing body parsers

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var oav = require('oav');
+// var oav = require('oav');
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var multer = require('multer');
+// var bodyParser = require('body-parser');
+// var multer = require('multer');
 // app.use(bodyParser.json()); // for parsing application/json
 // app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
